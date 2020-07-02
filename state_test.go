9e7f15c5b4281dc02389019a24cd2ed6e045b8e1
Fabian Bockaj <f.bockaj@mwaysolutions.com>
2020-07-02 12:48:34 +0200

    Improved state tests

--- state_test.go
+++ state_test.go
@@ -1,60 +1,54 @@
 package raftify
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"testing"
-
-	"github.com/hashicorp/memberlist"
 )
 
 func TestSaveLoadDeleteState(t *testing.T) {
-	pwd, _ := os.Getwd()
-	config := Config{
-		ID:       "Node_TestSaveLoadDeleteState",
-		MaxNodes: 3,
-		Expect:   1,
-		BindAddr: "0.0.0.0",
-		BindPort: 3000,
-		PeerList: []string{
-			"0.0.0.0:3001",
-			"0.0.0.0:3002",
-		},
-	}
-	node := &Node{
-		logger:     log.New(os.Stderr, "", 0),
-		workingDir: pwd,
-		config:     &config,
+	// Reserve ports for this test
+	ports := reservePorts(3)
+
+	// Initialize and start dummy node
+	node := initDummyNode("TestNode", 1, 1, ports[0])
+	node.config.PeerList = []string{
+		fmt.Sprintf("127.0.0.1:%v", ports[1]),
+		fmt.Sprintf("127.0.0.1:%v", ports[2]),
 	}
 
-	var err error
-	if node.memberlist, err = memberlist.Create(memberlist.DefaultWANConfig()); err != nil {
-		t.Logf("Expected creation of memberlist, instead got error: %v", err.Error())
+	node.createMemberlist()
+	defer node.memberlist.Shutdown()
+
+	// Fail loading the current node state
+	list, err := node.loadState()
+	if err == nil {
+		t.Log("Expected loadState to throw an error, instead it didn't")
 		t.FailNow()
 	}
 
+	// Save the current node state
 	node.saveState()
-	if _, err := os.Stat(pwd + "/state.json"); err != nil {
+	if _, err := os.Stat(node.workingDir + "/state.json"); err != nil {
 		t.Logf("Expected existing state.json, instead got error: %v", err.Error())
 		t.FailNow()
 	}
 
-	list, err := node.loadState()
+	// Succeed loading the current node state
+	list, err = node.loadState()
 	if err != nil {
-		t.Logf("Expected no errors during loadState, instead got: %v", err.Error())
+		t.Logf("Expected state.json to be loaded successfully, instead got error: %v", err.Error())
 		t.FailNow()
 	}
 	if len(list) != len(node.memberlist.Members()) {
-		t.Logf("Expected loaded list to be equal in size to the internal memberlist, instead got %v (loaded) and %v (internal)", len(list), len(node.memberlist.Members()))
+		t.Logf("Expected loaded list to be equal in size to the internal memberlist, instead got sizes %v (loaded) and %v (internal)", len(list), len(node.memberlist.Members()))
 		t.FailNow()
 	}
 
+	// Delete the current node state
 	node.deleteState()
-	if _, err := os.Stat(pwd + "/state.json"); err == nil {
+	if _, err := os.Stat(node.workingDir + "/state.json"); err == nil {
 		t.Log("Expected no state.json, instead it exists")
 		t.FailNow()
 	}
-
-	node.memberlist.Leave(0)
-	node.memberlist.Shutdown()
 }
